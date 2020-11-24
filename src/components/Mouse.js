e03c9a5a3f5a6ff7f31b2e13a0c9905998d0d9ef
henriB <henri.bretseau@gmail.com>
2020-11-24 20:48:43 +0100

    Mouse

---
+++ src/components/Mouse.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+const Mouse = () => {
+    useEffect( ()=>{
+        const handleCursor =(e)=>{
+
+        }
+        window.addEventListener("mousemove", handleCursor)
+    },[])
+    return (
+        <span className="cursor"></span>
+    );
+};
+
+export default Mouse;
