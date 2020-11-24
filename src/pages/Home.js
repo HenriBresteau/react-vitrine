cae4276928d0821f5a13bdd115341a0d4f11631c
henriB <henri.bretseau@gmail.com>
2020-11-24 17:55:55 +0100

    mise en forme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import Navigation from '../components/Navigation';
-import SocialNetwork from '../components/SocialNetwork';
+import React from "react";
+import DynamicText from "../components/DynamicText";
+import Navigation from "../components/Navigation";
+import SocialNetwork from "../components/SocialNetwork";
 
 const Home = () => {
-    return (
-        <div>
-            <Navigation></Navigation>
-            <SocialNetwork></SocialNetwork>
+  return (
+    <div>
+      <div className="home">
+        <Navigation></Navigation>
+        <SocialNetwork></SocialNetwork>
+        <div className="home-main">
+          <div className="main-content">
+            <h1>FS Acency</h1>
+            <h2>
+              <DynamicText></DynamicText>
+            </h2>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
