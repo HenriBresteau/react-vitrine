3c37dc6b6ad06c4d8911c56b0cedcb8421b2941a
henriB <henri.bretseau@gmail.com>
2020-11-24 20:55:35 +0100

    affichage cursor

--- src/components/Mouse.js
+++ src/components/Mouse.js
@@ -3,7 +3,8 @@ import React, { useEffect } from 'react';
 const Mouse = () => {
     useEffect( ()=>{
         const handleCursor =(e)=>{
-
+            document.querySelector('.cursor').style.top = e.pageY +"px";
+            document.querySelector('.cursor').style.left = e.pageX +"px";
         }
         window.addEventListener("mousemove", handleCursor)
     },[])
