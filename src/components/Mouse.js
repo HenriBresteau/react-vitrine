4abdb12d3c500fd63f78d4ba11fd87bb5def1fac
henriB <henri.bretseau@gmail.com>
2020-11-24 21:05:58 +0100

    handleHover + handleLeave

--- src/components/Mouse.js
+++ src/components/Mouse.js
@@ -7,6 +7,17 @@ const Mouse = () => {
             document.querySelector('.cursor').style.left = e.pageX +"px";
         }
         window.addEventListener("mousemove", handleCursor)
+        const handleHover = ()=> {
+            document.querySelector('.cursor').classList.add('hovered');
+        }
+        const handleLeave = ()=> {
+            document.querySelector('.cursor').style.transition =".3s ease-out";
+            document.querySelector('.cursor').classList.remove('hovered');
+        }
+        document.querySelectorAll('.hover').forEach((link)=>{
+            link.addEventListener('mouseover', handleHover);
+            link.addEventListener('mouseleave', handleLeave);
+        })
     },[])
     return (
         <span className="cursor"></span>
