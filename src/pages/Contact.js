b7da8a0c446901f1aae3792b4fbc0ef7b7a9db88
henriB <henri.bretseau@gmail.com>
2020-11-24 21:47:36 +0100

    structure

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import Logo from '../components/Logo';
 import Mouse from '../components/Mouse';
+import Navigation from '../components/Navigation';
 
 const Contact = () => {
     return (
-        <div>
+        <main>
             <Mouse></Mouse>
-            Contact
-        </div>
+            <div className="contact">
+                <Navigation></Navigation>
+                <Logo></Logo>
+                {/*contact form*/}
+                <div className="contact-infos">
+                    <div className="address">
+                        <div className="content">
+                            <h4>adresse</h4>
+                            <p>12 rue du Code</p>
+                            <p>69003 Lyon</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 };
 
