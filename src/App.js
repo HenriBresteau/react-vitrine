dd49e4cb771268e98eeb6031c4e72fe98e20369a
henriB <henri.bretseau@gmail.com>
2020-11-24 16:59:53 +0100

    add Pages + Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Porfolio from './pages/Porfolio';
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/project-1" component={Porfolio} />
+        <Route exact path="/contact" component={Contact} />
+        <Redirect to="/" />
+      </Switch>
+    
   );
 };
 
