af1ecf154bce90284342253fb54689b32eb11583
henriB <henri.bretseau@gmail.com>
2020-11-25 11:58:38 +0100

    visible + mise en forme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,50 +4,50 @@ import DynamicText from "../components/DynamicText";
 import Mouse from "../components/Mouse";
 import Navigation from "../components/Navigation";
 import SocialNetwork from "../components/SocialNetwork";
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 
 const Home = () => {
-
   const variants = {
     initial: {
-      opacity:0,
-      transition: {duration:0.5},
-      x:100,
+      opacity: 0,
+      transition: { duration: 0.5 },
+      x: 100,
     },
-    visible :{
-      opactiy:1,
-      x:0,
+    visible: {
+      opactiy: 1,
+      x: 0,
     },
     exit: {
-      opacity:0,
-      transition: {duration:0.3},
-      x:-100
-    }
-
-  }
+      opacity: 0,
+      transition: { duration: 0.3 },
+      x: -100,
+    },
+  };
 
   return (
     <main>
       <Mouse></Mouse>
-      <motion.div 
+      <motion.div
         className="home"
         initial="inital"
-        animate="animate"
+        animate="visible"
         exit="exit"
-        variants={variants}></motion.div>
-      <div className="home">
-        <Navigation></Navigation>
-        <SocialNetwork></SocialNetwork>
-        <div className="home-main">
-          <div className="main-content">
-            <h1>FS Acency</h1>
-            <h2>
-              <DynamicText></DynamicText>
-            </h2>
+        variants={variants}
+      >
+        <div className="home">
+          <Navigation></Navigation>
+          <SocialNetwork></SocialNetwork>
+          <div className="home-main">
+            <div className="main-content">
+              <h1>FS Acency</h1>
+              <h2>
+                <DynamicText></DynamicText>
+              </h2>
+            </div>
           </div>
+          <ButtonsBottom right={"/project-1"}></ButtonsBottom>
         </div>
-        <ButtonsBottom right={"/project-1"}></ButtonsBottom>
-      </div>
+      </motion.div>
     </main>
   );
 };
