34e778c950a110fc00ab19b01d6af1b9b97d697d
henriB <henri.bretseau@gmail.com>
2020-11-25 10:34:37 +0100

    <ContactFrom>

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,6 +5,7 @@ import Navigation from "../components/Navigation";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import SocialNetwork from "../components/SocialNetwork";
 import ButtonsBottom from "../components/ButtonsBottom";
+import ContactForm from "../components/ContactForm";
 
 const Contact = () => {
   return (
@@ -13,7 +14,7 @@ const Contact = () => {
       <div className="contact">
         <Navigation></Navigation>
         <Logo></Logo>
-        {/*contact form*/}
+        <ContactForm></ContactForm>
         <div className="contact-infos">
           <div className="address">
             <div className="content">
