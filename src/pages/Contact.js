db61c7c548bf47a899969a59bd9be789c7a6a4b5
henriB <henri.bretseau@gmail.com>
2020-11-25 09:52:11 +0100

    phone + copytoClipboard

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,28 +1,45 @@
-import React from 'react';
-import Logo from '../components/Logo';
-import Mouse from '../components/Mouse';
-import Navigation from '../components/Navigation';
+import React from "react";
+import Logo from "../components/Logo";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const Contact = () => {
-    return (
-        <main>
-            <Mouse></Mouse>
-            <div className="contact">
-                <Navigation></Navigation>
-                <Logo></Logo>
-                {/*contact form*/}
-                <div className="contact-infos">
-                    <div className="address">
-                        <div className="content">
-                            <h4>adresse</h4>
-                            <p>12 rue du Code</p>
-                            <p>69003 Lyon</p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <main>
+      <Mouse></Mouse>
+      <div className="contact">
+        <Navigation></Navigation>
+        <Logo></Logo>
+        {/*contact form*/}
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>Adresse</h4>
+              <p>12 rue du Code</p>
+              <p>69003 Lyon</p>
             </div>
-        </main>
-    );
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>Téléphone</h4>
+              <CopyToClipboard text="0612345678" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => {
+                    alert("Téléphone copié");
+                  }}
+                >
+                  06 12 34 56 78
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Contact;
