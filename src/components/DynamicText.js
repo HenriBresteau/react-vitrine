4660a59826011279bd5bd696bcd82cf4ded6f6d1
Henri Brestea <henri.bresteau@gmail.com>
2021-07-23 15:49:21 +0200

    change text

--- src/components/DynamicText.js
+++ src/components/DynamicText.js
@@ -1,59 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const DynamicText = () => {
+  useEffect(() => {
+    const target = document.getElementById("text-target");
+    let array = ["web", "SEO", "Front", "react"];
 
-    useEffect( ()=>{
-        const target = document.getElementById('text-target');
-        let array = ['simple', 'clear', 'smart', 'strong'];
-        
-        let wordIndex = 0;
-        let letterIndex = 0;
-        
-        const createLetter =() =>{
-            const letter = document.createElement('span');
-            target.appendChild(letter);
-        
-            letter.classList.add('letter');
-            letter.style.opacity = '0';
-            letter.style.animation= 'anim 6s ease forwards';
-            letter.textContent= array[wordIndex][letterIndex];
-            setTimeout(() => {
-               letter.remove() 
-            }, 2000);
-        }
-        const loop = ()=> {
-            setTimeout(() => {
-                if (wordIndex >= array.length) {
-                    wordIndex=0;
-                    letterIndex=0;
-                    loop();
-                }
-                else if (letterIndex < array[wordIndex].length) {
-                    createLetter();
-                    letterIndex++;
-                    loop();
-                } else{
-                    letterIndex=0;
-                    wordIndex++;
-                    setTimeout(() => {
-                        loop();
-                    }, 2000);
-                    
-                }
-            }, 100);
-        }
-        
-        loop();
+    let wordIndex = 0;
+    let letterIndex = 0;
 
-    })
-    return (
-        <span className="dynamic-text">
-            <span className="simply">
-                simply
-            </span>
-            <span id="text-target"></span>
-        </span>
-    );
+    const createLetter = () => {
+      const letter = document.createElement("span");
+      target.appendChild(letter);
+
+      letter.classList.add("letter");
+      letter.style.opacity = "0";
+      letter.style.animation = "anim 6s ease forwards";
+      letter.textContent = array[wordIndex][letterIndex];
+      setTimeout(() => {
+        letter.remove();
+      }, 2000);
+    };
+    const loop = () => {
+      setTimeout(() => {
+        if (wordIndex >= array.length) {
+          wordIndex = 0;
+          letterIndex = 0;
+          loop();
+        } else if (letterIndex < array[wordIndex].length) {
+          createLetter();
+          letterIndex++;
+          loop();
+        } else {
+          letterIndex = 0;
+          wordIndex++;
+          setTimeout(() => {
+            loop();
+          }, 2000);
+        }
+      }, 100);
+    };
+
+    loop();
+  });
+  return (
+    <span className="dynamic-text">
+      <span className="simply">Développeur</span>
+      <span id="text-target"></span>
+    </span>
+  );
 };
 
 export default DynamicText;
