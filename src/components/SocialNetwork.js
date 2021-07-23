a238b29d6967041b5722c906a8b02e7e97a69a1b
Henri Brestea <henri.bresteau@gmail.com>
2021-07-23 15:49:33 +0200

    change for linkedin

--- src/components/SocialNetwork.js
+++ src/components/SocialNetwork.js
@@ -1,36 +1,59 @@
-import React from 'react';
+import React from "react";
 
 const SocialNetwork = () => {
+  const anim = () => {
+    let navLinks = document.querySelectorAll(".social-network a");
+    navLinks.forEach((link) => {
+      link.addEventListener("mouseover", (e) => {
+        let attrX = e.offsetX - 20;
+        let attrY = e.offsetY - 13;
 
-    const anim = () =>{
-        let navLinks = document.querySelectorAll('.social-network a');
-        navLinks.forEach( link => {
-            link.addEventListener('mouseover',(e)=>{
-                let attrX= e.offsetX - 20;
-                let attrY= e.offsetY - 13;
-
-                link.style.transform = `translate(${attrX}px, ${attrY}px)`
-            })
-            link.addEventListener('mouseleave', ()=>{
-                link.style.transform ='';
-            })
-        })
-    }
-    return (
-        <div className="social-network">
-            <ul className="content">
-                <a href="http://www.facebook.com" target="_blank" rel="noopener noreferrer" className="hover" onMouseOver={anim} >
-                    <li> <i className="fab fa-facebook-f"></i> </li>
-                </a>
-                <a href="http://www.twiiter.com" target="_blank" rel="noopener noreferrer" className="hover" onMouseOver={anim} >
-                    <li> <i className="fab fa-twitter"></i> </li>
-                </a>
-                <a href="http://www.instagram.com" target="_blank" rel="noopener noreferrer" className="hover" onMouseOver={anim}>
-                    <li> <i className="fab fa-instagram"></i> </li>
-                </a>
-            </ul>
-        </div>
-    );
+        link.style.transform = `translate(${attrX}px, ${attrY}px)`;
+      });
+      link.addEventListener("mouseleave", () => {
+        link.style.transform = "";
+      });
+    });
+  };
+  return (
+    <div className="social-network">
+      <ul className="content">
+        <a
+          href="http://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover"
+          onMouseOver={anim}
+        >
+          <li>
+            <i className="fab fa-facebook-f"></i>
+          </li>
+        </a>
+        <a
+          href="http://www.twiiter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover"
+          onMouseOver={anim}
+        >
+          <li>
+            <i className="fab fa-twitter"></i>
+          </li>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/henri-bresteau/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover"
+          onMouseOver={anim}
+        >
+          <li>
+            <i className="fab fa-linkedin"></i>
+          </li>
+        </a>
+      </ul>
+    </div>
+  );
 };
 
 export default SocialNetwork;
